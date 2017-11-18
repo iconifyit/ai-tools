1a439df348de45fd4f3c26a26bf8a981b1ec7b44
Scott Lewis <scott@iconify.it>
2017-11-18 08:31:05 -0500

    Delete unnecessary assets

--- _app.command
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /Users/scott/github/iconify/ai-tools/
-echo "Un-zipping META file"
-node app.js
--- _meta.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    inputfile: '/Users/scott/Desktop/avatars-iconjar/Iconjar-avatars/robots.iconjar/META',
-	outputfile: '/Users/scott/Desktop/avatars-iconjar/Iconjar-avatars/META.json'
-};
