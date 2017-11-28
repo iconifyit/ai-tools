7488a7bad0d695eb2430fe555ceeb87dea80b553
Scott Lewis <scott@iconify.it>
2017-11-28 08:22:31 -0500

    Add META.json

--- var/meta.js
+++ var/meta.js
@@ -1,1 +1,1 @@
-module.exports = {"inputfile":"/Users/scott/Desktop/avatars-iconjar/Iconjar-avatars/robots.iconjar/META","outputfile":"/Users/scott/Desktop/avatars-iconjar/Iconjar-avatars/META.json"}
+module.exports = {  "inputfile": "/Users/scott/Desktop/avatars-iconjar/Iconjar-avatars/robots.iconjar/META",  "outputfile": "/Users/scott/github/iconify/ai-tools//var/META.json"}
